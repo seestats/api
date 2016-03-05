871cc874f088d6fcc6db31f241114cf1b3e105ac
Gediminas Ivanauskas <gediminas.iv@gmail.com>
2016-03-05 22:14:16 +0200

    Change controller function to support custom size

--- controllers/requests.js
+++ controllers/requests.js
@@ -3,9 +3,9 @@
 const dateHelper = require(`${__dirname}/../helpers/dateHelper`);
 const requestHelper = require(`${__dirname}/../helpers/requestHelper`);
 
-exports.getRowRequest = (field, req, res) => {
+exports.getRowRequest = (field, size, req, res) => {
   if (dateHelper.isValidDate(req.query.from) && dateHelper.isValidDate(req.query.to)) {
-    requestHelper.makeAllRowRequest(field, req.query.from, req.query.to, res);
+    requestHelper.makeAllRowRequest(field, size, req.query.from, req.query.to, res);
   } else {
     res.json({ success: false, message: 'Bad date formatting' });
   }
