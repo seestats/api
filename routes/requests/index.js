3547e66fa855dd7ae44b5e6ebd6a617585e6dcef
Gediminas Ivanauskas <gediminas.iv@gmail.com>
2016-03-05 20:33:16 +0200

    Add access control header that allows origin

--- routes/requests/index.js
+++ routes/requests/index.js
@@ -4,6 +4,7 @@ const requestHelper = require('../../helpers/requestHelper.js');
 module.exports = {
   '/:request/top10': {
     get: function get(req, res, cb) {
+    	res.setHeader('Access-Control-Allow-Origin', '*');
     	requestsController.getRowRequest(req.params.request, req, res);
     },
   },
